6373b5b020c498b8eee5cdf8174771eedf1d1bff
josipkramar <josip.kramar@fpmoz.sum.ba>
2025-05-23 02:06:32 +0200

    Reset passwrod (activity, layout)

---
+++ app/src/main/java/com/fpmoz/travelmate/ResetPasswordActivity.kt
@@ -0,0 +1,44 @@
+package com.fpmoz.travelmate
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class ResetPasswordActivity : AppCompatActivity() {
+
+    private lateinit var resetEmailTxt: EditText
+    private lateinit var resetPasswordBtn: Button
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+
+        resetEmailTxt = findViewById(R.id.resetEmailTxt)
+        resetPasswordBtn = findViewById(R.id.resetPasswordBtn)
+
+
+        auth = FirebaseAuth.getInstance()
+
+        resetPasswordBtn.setOnClickListener {
+            val email = resetEmailTxt.text.toString().trim()
+
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Please enter your email", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Reset link sent to your email", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(this, "Error: ${task.exception?.message}", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+}
